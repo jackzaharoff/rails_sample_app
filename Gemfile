17b9c745c65a9ac03175fab879d453df37609f7a
Jack Zaharoff <jw8006@gmail.com>
2013-05-27 21:40:16 -0400

    Add Cucumber features

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'faker', '1.1.2'
 gem 'will_paginate', '3.0.4'
 gem 'bootstrap-will_paginate', '0.0.9'
 gem 'jquery-rails', '2.2.1'
+gem 'json', '1.8.0'
 
 group :development, :test do
   gem 'rspec-rails', '2.13.2'
--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -15,14 +15,12 @@ describe "AuthenticationPages" do
     describe "with invalid information" do
       before { click_button 'Sign in' }
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
-
+      it { should have_error_message }
       describe 'after visiting another page' do
         before { click_link 'Home' }
-        it { should_not have_selector('div.alert.alert-error') }
+        it { should_not have_selector('div.alert.alert-error', text: 'Invalid') }
       end
     end
-
     describe 'with valid information' do
       let(:user) { FactoryGirl.create(:user) }
       before do
